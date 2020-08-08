229be34daa41a177535eefdabfa8e4da50fc6651
vegeta <944031211@qq.com>
2020-08-08 22:52:28 +0800

    出现does not have a commit checked out问题错误后的重新提交

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Vegeta Michael on 2020/8/8.
+//  Copyright © 2020 Vegeta Michael. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
